c350ebfd199862bd87aebc1ffee6ca0e0b3243f5
nishamokashe <nishamokase18@gmail.com>
2023-08-16 22:49:32 +0530

    globalObject.js
    globalObject.js

---
+++ globalObject.js
@@ -0,0 +1,15 @@
+//GlobalObject- that are avilable thoughout your application importing
+
+// console.log(global);
+
+// console.log(process);
+
+// console.log(console);
+
+// console.log(module);
+
+console.log(setTimeout);
+
+console.log(__dirname);
+
+console.log(__filename);
